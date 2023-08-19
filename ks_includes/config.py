e4ff3d889a8481a84fe997ce72913cafa9280b77
Zeanon <thezeanon@gmail.com>
2023-08-19 15:06:35 +0200

    .

--- ks_includes/config.py
+++ ks_includes/config.py
@@ -176,7 +176,8 @@ class KlipperScreenConfig:
                 strs = (
                     'moonraker_api_key', 'moonraker_host', 'titlebar_name_type',
                     'screw_positions', 'power_devices', 'titlebar_items', 'z_babystep_values',
-                    'extrude_distances', "extrude_speeds", 'z_calibrate_panel', 'lighting_output_pins'
+                    'extrude_distances', "extrude_speeds", 'z_calibrate_panel', 'camera_url',
+                    'lighting_output_pins'
                 )
                 numbers = (
                     'moonraker_port', 'move_speed_xy', 'move_speed_z',
@@ -202,9 +203,9 @@ class KlipperScreenConfig:
             for key in config[section]:
                 if key not in bools and key not in strs and key not in numbers:
                     msg = f'Option "{key}" not recognized for section "[{section}]"'
-                    if key == "camera_url":
-                        msg = "camera_url has been deprecated in favor of moonraker cameras"
-                        msg += "\n\n https://moonraker.readthedocs.io/en/latest/configuration/#webcam"
+                    # if key == "camera_url":
+                    #     msg = "camera_url has been deprecated in favor of moonraker cameras"
+                    #     msg += "\n\n https://moonraker.readthedocs.io/en/latest/configuration/#webcam"
                     if remove:
                         # This should only be called for the auto-generated section
                         self.config.remove_option(section, key)
