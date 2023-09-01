5c1ce492ad9aba3636a218bf8b913d10b0bd321b
Zeanon <thezeanon@gmail.com>
2023-09-01 18:48:26 +0200

    .

--- screen.py
+++ screen.py
@@ -877,9 +877,10 @@ class KlipperScreen(Gtk.Window):
         else:
             self.z_calibrate_panel = (printer_config
                                       .get("z_calibrate_panel", "zcalibrate"))
-            self.lighting_output_pins = map(str.strip, printer_config
-                                            .get("lighting_output_pins", "caselight")
-                                            .split(','))
+            self.lighting_output_pins = (printer_config
+                                         .get("lighting_output_pins", "caselight")
+                                         .split(','))
+            self.lighting_output_pins = [s.strip() for s in self.lighting_output_pins]
         self.base_panel.set_ks_printer_cfg(self.connected_printer)
 
         # Moonraker is ready, set a loop to init the printer
