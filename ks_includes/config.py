edb5d7355ef9fac1643e04abe677cf3d62ede0d3
Zeanon <thezeanon@gmail.com>
2023-10-12 19:36:33 +0200

    Update config.py

--- ks_includes/config.py
+++ ks_includes/config.py
@@ -159,7 +159,7 @@ class KlipperScreenConfig:
                 bools = (
                     'invert_x', 'invert_y', 'invert_z', '24htime', 'only_heaters', 'show_cursor', 'confirm_estop',
                     'autoclose_popups', 'use_dpms', 'use_default_menu', 'side_macro_shortcut', 'use-matchbox-keyboard',
-                    'show_heater_power', "show_scroll_steppers", "auto_open_extrude"
+                    'show_heater_power', "show_scroll_steppers", "auto_open_extrude", "enable_home_full"
                 )
                 strs = (
                     'default_printer', 'language', 'print_sort_dir', 'theme', 'screen_blanking', 'font_size',
