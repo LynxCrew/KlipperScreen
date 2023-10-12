65098c2910e92a3f9162c50128a54d0b68a9b7a6
Zeanon <thezeanon@gmail.com>
2023-10-12 10:01:03 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -897,7 +897,7 @@ class KlipperScreen(Gtk.Window):
             for element in printer_config.get("lighting_output_pins", "caselight: 100").split(','):
                 pair = [p.strip() for p in element.strip().split(':')]
                 if len(pair) == 1:
-                    pair.append(100)
+                    pair.append(1000)
                 elif len(pair) == 2:
                     if not pair[1].replace(".", "").isnumeric():
                         logging.error(f"[{pair[1]}] in lighting_output_pins is not a number")
