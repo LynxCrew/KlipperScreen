3c3fdbde7847da7a40786cdb55471e8c198cb1df
Zeanon <thezeanon@gmail.com>
2024-04-27 02:52:32 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -287,6 +287,7 @@ class KlipperScreen(Gtk.Window):
                 "screws_tilt_adjust": ['results', 'error'],
             }
         }
+        logging.info("TEMP FANS")
         logging.info(self.printer.get_temp_fans())
         for extruder in self.printer.get_tools():
             requested_updates['objects'][extruder] = [
