caf8bbdb3e2cdc9df2e46522d846f4ce80300dab
Zeanon <thezeanon@gmail.com>
2024-06-22 11:01:23 +0200

    Update zcalibrate_lynxbot.py

--- panels/zcalibrate_lynxbot.py
+++ panels/zcalibrate_lynxbot.py
@@ -30,7 +30,7 @@ class Panel(ScreenPanel):
         if self.probe:
             self.x_offset = float(self.probe['x_offset']) if "x_offset" in self.probe else 0.0
             self.y_offset = float(self.probe['y_offset']) if "y_offset" in self.probe else 0.0
-            self.z_offset = float(self.probe['z_offset']) if "z_offset" in self.probe else 0.0
+            self.z_offset = float(self.probe['z_offset']) if "z_offset" in self.probe else None
             if "sample_retract_dist" in self.probe:
                 self.z_hop = float(self.probe['sample_retract_dist'])
             if "speed" in self.probe:
