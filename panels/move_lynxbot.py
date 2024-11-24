8202efda433c0a1fa364a8923cf71ef4e8a1be75
Zeanon <thezeanon@gmail.com>
2024-11-24 13:57:43 +0100

    Update move_lynxbot.py

--- panels/move_lynxbot.py
+++ panels/move_lynxbot.py
@@ -233,7 +233,7 @@ class Panel(ScreenPanel):
         name = "homing"
         disname = self._screen._config.get_menu_name("move", name)
         menuitems = self._screen._config.get_menu_items("move", name)
-        logging.info(self._printer.get_stat("z_tilt_ng", "applied"))
+        logging.info(self._printer.get_stat("z_tilt_ng"))
         self._screen.show_panel("menu", disname, items=menuitems, panel_name="Homing")
 
     def motors_off(self, widget):
