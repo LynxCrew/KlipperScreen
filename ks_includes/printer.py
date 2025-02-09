b0d5a0f73751626b3b8e66b70fb1355e738482be
TheZeanon <thezeanon@gmail.com>
2025-02-09 17:28:41 +0100

    Update printer.py

--- ks_includes/printer.py
+++ ks_includes/printer.py
@@ -120,6 +120,8 @@ class Printer:
             self.store_timeout = None
 
     def process_force_query(self, result, method, params):
+        if result is None:
+            return
         self.process_update(result["result"]["status"])
 
     def process_update(self, data):
